8fc8028d5c4d67ece166a14f415fb94dcad81a04
Samuel Hermany <samuelhermany@hotmail.com>
2025-04-18 20:26:18 -0300

    AT - Quastão 1 estava incorreta segundo enunciado

--- AT/src/main/java/exercicio_01/Exercicio_01.java
+++ AT/src/main/java/exercicio_01/Exercicio_01.java
@@ -4,6 +4,7 @@ public class Exercicio_01 {
     /// Instalando e configurando o ambiente Java
     public void Start()
     {
-        System.out.println("Hello World!");
+        String nome = "Samuel Hermany";
+        System.out.println("OLlá, menu nome é "+ nome + " e estou aprendendo Java!");
     }
 }
